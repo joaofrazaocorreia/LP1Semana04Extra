4f52b7537a83381479086c0ebefcf42b20d3b753
Nuno Fachada <nuno.fachada@ulusofona.pt>
2019-03-22 08:06:11 +0000

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testrepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
